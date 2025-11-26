01e1b0c0feefd9f3e9237a4f9b11ee217ca5ae27
Eduardo Fernandez <fernandez9000@gmail.com>
2025-11-25 19:33:30 -0800

    feat: integrate rusqlite for dog image storage and add database setup

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,25 @@ use serde::*;
 
 static CSS: Asset = asset!("/assets/main.css");
 
+#[cfg(feature = "server")]
+thread_local! {
+    pub static DB: rusqlite::Connection = {
+        // Open the database from the persisted "hotdog.db" file
+        let conn = rusqlite::Connection::open("hotdog.db").expect("Failed to open database");
+
+        // Create the "dogs" table if it doesn't already exist
+        conn.execute_batch(
+            "CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                url TEXT NOT NULL
+            );",
+        ).unwrap();
+
+        // Return the connection
+        conn
+    };
+}
+
 fn main() {
     #[cfg(not(feature = "server"))]
     dioxus::launch(App);
@@ -43,21 +62,29 @@ struct SaveDogArgs {
     image: String,
 }
 
+#[server]
+async fn save_dog_in_db(image: String) -> Result<()> {
+    DB.with(|f| f.execute("INSERT INTO dogs (url) VALUES (?1)", &[&image]))?;
+    Ok(())
+}
+
 #[post("/api/save_dog")]
 async fn save_dog(Json(args): Json<SaveDogArgs>) -> Result<()> {
     tracing::info!("Received image to save: {}", args.image);
 
-    use std::io::Write;
+    // use std::io::Write;
 
-    let mut file = std::fs::OpenOptions::new()
-        .write(true)
-        .append(true)
-        .create(true)
-        .open("dogs.txt")
-        .unwrap();
+    // let mut file = std::fs::OpenOptions::new()
+    //     .write(true)
+    //     .append(true)
+    //     .create(true)
+    //     .open("dogs.txt")
+    //     .unwrap();
 
-    // And then write a newline to it with the image url
-    file.write_fmt(format_args!("{}\n", args.image));
+    // // And then write a newline to it with the image url
+    // file.write_fmt(format_args!("{}\n", args.image));
+
+    save_dog_in_db(args.image).await?;
 
     Ok(())
 }
